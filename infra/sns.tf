805301d448bba651e2e4af3ef1cba1071d056199
Goody27 <b0531060@eng.kitakyu-u.ac.jp>
2026-05-15 17:59:34 +0900

    chore: empty all tf resources to trigger destroy via apply

--- infra/sns.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_sns_topic" "escalation" {
-  name = "mymom-escalation"
-}
--- infra/ssm.tf
+++
@@ -1,3 +0,0 @@
-# SSM Parameter Store はこのプロジェクトでは使用しない。
-# Slack Token/Secret は var.slack_bot_token_arn / var.slack_signing_secret_arn (input variable) で渡す。
-# Bedrock Agent ID 等は bedrock.tf のリソース参照で取得する。
